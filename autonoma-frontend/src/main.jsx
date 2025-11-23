5db4c6911e4f2643657d5df9a997f89fd7a6af34
JD17VM <jdiegodiego4@gmail.com>
2025-11-23 13:57:37 -0500

    :lipstick: feat(styles): add hybrid css reset combining normalize and meyer rules

--- autonoma-frontend/src/main.jsx
+++ autonoma-frontend/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './assets/styles/normalize.css'
+
 import './index.css'
 import App from './App.jsx'
 
